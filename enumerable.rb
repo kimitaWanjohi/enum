86539d2f4ef205a2299fc0091ce8c0df901e642d
Kimita Wanjohi <kimitawanjohi7923@gmail.com>
2023-08-24 07:30:49 +0000

    Add enumerable module

---
+++ enumerable.rb
@@ -0,0 +1,14 @@
+module MyEnumerable
+  def all?
+    each { |item| return false unless yield(item) }
+    true
+  end
+
+  def any?
+    each { |item| return true if yield(item) }
+    false
+  end
+
+
+  end
+end
